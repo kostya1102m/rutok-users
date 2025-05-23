5ff7d947d28c9292da4de6e9f63b687a5d02b758
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-05-23 22:38:31 +1000

    init engine settings

---
+++ app/db/db.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from config import settings
+
+DATABASE_URL = settings.get_db_url()
+
+engine = create_async_engine(url=DATABASE_URL, echo=True)
+
+async_session = async_sessionmaker(engine, expire_on_commit=False)
