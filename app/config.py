9523b1b61773a55d7b82795417345f612a7a1294
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-05-23 22:22:11 +1000

    init commit

---
+++ app/config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+
+env_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env")
+load_dotenv(dotenv_path=env_path)
+
+class Settings:
+    def __init__(self):
+        self.DB_USER = os.getenv("DB_USER")
+        self.DB_PASSWORD = os.getenv("DB_PASSWORD")
+        self.DB_HOST = os.getenv("DB_HOST")
+        self.DB_PORT = os.getenv("DB_PORT")
+        self.DB_NAME = os.getenv("DB_NAME")
+        self.SECRET_KEY = os.getenv("SECRET_KEY")
+
+    def get_db_url(self):
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+settings = Settings()
