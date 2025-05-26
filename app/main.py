e3df8152b95a3f3c575d957f3ed00f7c4b5f29c9
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-05-27 01:33:11 +1000

    add logger

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from api.v1.user import router as user
 from api.v1.role import router as role
+from logging_settings import configure_logging
+import logging
+
 app = FastAPI()
 
+configure_logging()
+
+logger = logging.getLogger(__name__)
 
 app.include_router(user, prefix="/api/v1")
 app.include_router(role, prefix="/api/v1")
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
