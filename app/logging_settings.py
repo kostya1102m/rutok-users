be3675dd406ace09dbab6b6417eee85dce18fa88
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-05-27 01:26:15 +1000

    set up logging settings

--- app/logging_settings.py
+++ app/logging_settings.py
@@ -1,9 +1,29 @@
 import logging
-
+from logging.handlers import RotatingFileHandler
+import os
 
 def configure_logging(level=logging.INFO):
+    
+    dir = "logs"
+    
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        
+    log_file = os.path.join(dir, "app.log")
+    
+    log_format = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s %(message)s"
+    date_format = "%Y-%m-%d %H:%M:%S"
+    
     logging.basicConfig(
         level=level,
-        format="[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+        format=log_format,
+        datefmt=date_format,
+        handlers=[
+            RotatingFileHandler(log_file, maxBytes=1000000, backupCount=5),
+            logging.StreamHandler()
+        ]
     )
+    
+    logger = logging.getLogger(__name__)
+    logger.info("Logging configured")
+    return logger
