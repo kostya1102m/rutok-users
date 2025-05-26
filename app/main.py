facd31623b929f0236dfd8255d2f4c116a722a8a
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-05-27 00:55:20 +1000

    add user, role to main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-
+from api.v1.user import router as user
+from api.v1.role import router as role
 app = FastAPI()
 
+
+app.include_router(user)
+app.include_router(role)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
