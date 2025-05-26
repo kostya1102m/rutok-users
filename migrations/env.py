6cf891f5f8900941bd1f4c597b22cf6649f1a301
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-05-27 00:52:31 +1000

    fix import

--- migrations/env.py
+++ migrations/env.py
@@ -6,8 +6,8 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from app.db.db import DATABASE_URL
-from app.schemas.tables import Base
+from app.database import DATABASE_URL
+from app.db.tables import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
