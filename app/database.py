8c4f65ea6ef53f88f463bb1da869ce83f4e31210
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-05-27 08:05:56 +1000

    fix import

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
-from config import settings
+from app.config import settings
 
 DATABASE_URL = settings.get_db_url()
 
