3ed46991e3753a7dba4cf678d5037405120b6f81
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-05-27 00:54:11 +1000

    removed

--- app/db/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from app.config import settings
-
-DATABASE_URL = settings.get_db_url()
-
-engine = create_async_engine(url=DATABASE_URL, echo=True)
-
-async_session = async_sessionmaker(engine, expire_on_commit=False)
