0b3c94fd40b7899addedd62c906b42f78c293e58
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-05-29 08:45:30 +1000

    add register router

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -1,11 +1,10 @@
-from fastapi import Depends, APIRouter, Request, Response
-from starlette.responses import HTMLResponse
+from fastapi import Depends, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from services.user_service import UserService
 from repository.user_repository import UserRepository
-from models.user import UserCreate
+from models.user import UserRegister, UserSchema
 from database import get_db
 
 router = APIRouter(
@@ -18,14 +17,14 @@ async def get_user_service():
     return UserService(user_repository)
 
 
-@router.get("/")
+@router.get("/", response_model=list[UserSchema])
 async def get_users(
     session: AsyncSession = Depends(get_db),
     user_service: UserService = Depends(get_user_service)
 ):
     return await user_service.get_user_all(session)
 
-@router.get("/{id}")
+@router.get("/{id}", response_model=UserSchema)
 async def get_user_by_id(
     id: int,
     session: AsyncSession = Depends(get_db),
@@ -33,7 +32,7 @@ async def get_user_by_id(
 ):
     return await user_service.get_user_by_id(id, session)
 
-@router.get("/email/{email}")
+@router.get("/email/{email}", response_model=UserSchema)
 async def get_user_by_email(
     email: str,
     session: AsyncSession = Depends(get_db),
@@ -41,7 +40,7 @@ async def get_user_by_email(
 ):
     return await user_service.get_user_by_email(email, session)
 
-@router.get("/username/{username}")
+@router.get("/username/{username}", response_model=UserSchema)
 async def get_user_by_username(
     username: str,
     session: AsyncSession = Depends(get_db),
@@ -49,13 +48,13 @@ async def get_user_by_username(
 ):
     return await user_service.get_user_by_username(username, session)
 
-@router.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(
-    userCreate: UserCreate,
+@router.post("/register/", status_code=status.HTTP_201_CREATED)
+async def register_user(
+    userRegister: UserRegister,
     session: AsyncSession = Depends(get_db),
     user_service: UserService = Depends(get_user_service)
 ):
-    return await user_service.create_user(userCreate, session)
+    return await user_service.register_user(userRegister, session)
 
 @router.delete("/delete/{id}", status_code=status.HTTP_200_OK)
 async def delete_user_by_id(
