dc29bf4f6447e6f68b92462c8a2378ca3420539c
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-05-29 09:00:01 +1000

    add exception handler for pydantic validation error

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette import status
+
 from api.v1.user import router as user
 from api.v1.role import router as role
 from logging_settings import configure_logging
@@ -12,6 +16,14 @@ logger = logging.getLogger(__name__)
 
 app.include_router(user, prefix="/api/v1")
 app.include_router(role, prefix="/api/v1")
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc: RequestValidationError):
+    error_msg = exc.errors()[0]["msg"]
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"detail": error_msg}
+    )
 
 @app.get("/")
 async def root():
