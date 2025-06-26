3d22e68dfb561e449cf2605bf6ce5eae275a5b87
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-06-26 22:10:26 +1000

    clear code, delete api versioning

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette import status
 
-from api.v1.user import router as user
-from api.v1.role import router as role
-from logging_settings import configure_logging
+from app.api.v1.user import router as user
+from app.api.v1.role import router as role
+from app.logging_settings import configure_logging
 import logging
 
 app = FastAPI()
@@ -14,8 +14,8 @@ configure_logging(level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 
-app.include_router(user, prefix="/api/v1")
-app.include_router(role, prefix="/api/v1")
+app.include_router(user, prefix="/api")
+app.include_router(role, prefix="/api")
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc: RequestValidationError):
